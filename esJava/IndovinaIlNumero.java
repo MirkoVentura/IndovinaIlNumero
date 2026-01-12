d55c9e26b7c6479adb5e1f202c2f64b2832046f2
RoccoLau08 <roccolaurino08@gmail.com>
2026-01-12 11:59:17 +0100

    Versionamento tentativi finiti

--- esJava/IndovinaIlNumero.java
+++ esJava/IndovinaIlNumero.java
@@ -1,10 +1,12 @@
 import java.util.Random;
-import java.util.Scanner;
+import java.io.*;
 
 public class IndovinaIlNumero {
+    static BufferedReader buffer;
+
     public static void main(String[] args) {
         Random random = new Random();
-        Scanner scanner = new Scanner(System.in);
+        buffer = new BufferedReader(new InputStreamReader(System.in));
         
         int numeroDaIndovinare = random.nextInt(100) + 1; // numero tra 1 e 100
         int tentativi = 0;
@@ -12,24 +14,37 @@ public class IndovinaIlNumero {
         
         System.out.println("=== INDOVINA IL NUMERO ===");
         System.out.println("Ho pensato un numero tra 1 e 100. Prova a indovinarlo!");
-        
-        while (true) {
-            System.out.print("\nInserisci il tuo tentativo: ");
-            tentativo = scanner.nextInt();
-            tentativi++;
-            
-            if (tentativo < numeroDaIndovinare) {
-                System.out.println("Troppo basso! Riprova.");
-            } else if (tentativo > numeroDaIndovinare) {
-                System.out.println("Troppo alto! Riprova.");
-            } else {
-                System.out.println("\n :) Complimenti! Hai indovinato!");
-                System.out.println("Il numero era: " + numeroDaIndovinare);
-                System.out.println("Tentativi impiegati: " + tentativi);
-                break;
+      
+        do{
+
+                tentativo = leggiInt("\nInserisci il tuo tentativo:");
+                tentativi++;
+                
+                if (tentativo < numeroDaIndovinare) {
+                    System.out.println("Troppo basso! Riprova.");
+                } else if (tentativo > numeroDaIndovinare) {
+                    System.out.println("Troppo alto! Riprova.");
+                } else {
+                    System.out.println("\n :) Complimenti! Hai indovinato!");
+                    System.out.println("Il numero era: " + numeroDaIndovinare);
+                    System.out.println("Tentativi impiegati: " + tentativi);
+                    break;
+                }
+        }while(tentativi < 10);
+        System.out.println("Tentativi terminati");
+    
+    }
+
+    public static int leggiInt(String msg){
+        int n = 0;
+        while(true){
+            System.out.print(msg);
+            try{
+                n = Integer.parseInt(buffer.readLine());
+            }catch(Exception e){
+                System.out.println("Inserisci numero");
             }
+            return n;
         }
-        
-        scanner.close();
     }
 }
